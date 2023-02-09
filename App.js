8c000ca12adb671eab9675ce6dc60c23f9dd312f
akhilneha <90970355+akhilneha@users.noreply.github.com>
2023-02-09 19:12:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation"; 
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./screens/home";
+import DetailsScreen from "./screens/details";
+
+export default function App() { 
+    return <AppContainer/>
+  ;
+}
+
+const appStackNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        headerShown: false
+      }
+    },
+    Details: {
+      screen: DetailsScreen
+    }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(appStackNavigator);
